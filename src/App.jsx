8f0cf79df853f7024d72aa34c4721af04c3e3b7d
ZeroDeath2 <harinchirappannath@gmail.com>
2024-10-27 19:03:40 +0530

    vite stupid error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { supabase } from "./supabase"; // Import Supabase client
 import Login from "./components/login";
 import ProjectList from "./components/projectlist";
 import ProjectView from "./components/projectview";
-import Layout from "./components/Layout"; // Assuming you have a Layout component
-
 const App = () => {
   const [userId, setUserId] = useState(null); // State to hold the user ID
 
@@ -25,30 +23,28 @@ const App = () => {
 
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <Login
-                onUserIdChange={handleUserIdChange}
-                onLogout={handleLogout}
-              />
-            }
-          />
-          <Route
-            exact
-            path="/projects"
-            element={<ProjectList userId={userId} onLogout={handleLogout} />}
-          />
-          <Route
-            exact
-            path="/projects/:id"
-            element={<ProjectView onLogout={handleLogout} />}
-          />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <Login
+              onUserIdChange={handleUserIdChange}
+              onLogout={handleLogout}
+            />
+          }
+        />
+        <Route
+          exact
+          path="/projects"
+          element={<ProjectList userId={userId} onLogout={handleLogout} />}
+        />
+        <Route
+          exact
+          path="/projects/:id"
+          element={<ProjectView onLogout={handleLogout} />}
+        />
+      </Routes>
     </Router>
   );
 };
